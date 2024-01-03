3ae189ec060be0f02d2aa454ded4514911a7de52
Griffin Stout <32140126+gdstout@users.noreply.github.com>
2024-01-03 15:21:43 -0600

    pick random image from bucket

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 console.log("HELLO FROM AMPLIFY");
 
-import {ACCESS_KEY_ID, SECRET_ACCESS_KEY} from "./config.js";
+import { ACCESS_KEY_ID, SECRET_ACCESS_KEY } from "./config.js";
 
 
 AWS.config.update({
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    accessKeyId: ACCESS_KEY_ID,
+    secretAccessKey: SECRET_ACCESS_KEY,
     region: 'us-east-1',
 });
 
@@ -13,19 +13,28 @@ const s3 = new AWS.S3();
 
 const bucketName = 'griffinsphotos';
 
-function listObjects() {
+let bucketObjects = [];
+
+function getObjects() {
     const params = { Bucket: bucketName };
 
     s3.listObjects(params, function (err, data) {
         if (err) {
             console.error('Error listing objects: ', err);
         } else {
-            console.log('Objects in the bucket: ', data.Contents);
-            // Process the objects as needed
-		
+            bucketObjects = data.Contents;
+            pickRandomImg();
         }
     });
 }
 
-// Call the function to list objects when the page loads
-listObjects();
+function pickRandomImg() {
+    if (bucketObjects.length) {
+        let index = Math.floor(Math.random() * bucketObjects.length);
+        const url = "https://griffinsphotos.s3.amazonaws.com/" + bucketObjects[index].Key;
+
+        document.getElementById("main-image").src = url;
+    }
+}
+
+getObjects();
