410f86b1a742f681282da117172fe425c90d5e7d
Griffin Stout <32140126+gdstout@users.noreply.github.com>
2024-01-03 14:45:45 -0600

    update with config

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import {ACCESS_KEY_ID, SECRET_ACCESS_KEY} from "./config.js";
+
+
 AWS.config.update({
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    accessKeyId: ACCESS_KEY_ID,
+    secretAccessKey: SECRET_ACCESS_KEY,
     region: 'us-east-1',
 });
 
