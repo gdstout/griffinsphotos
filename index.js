45495e21029ed7b35bf5b3a5c88023ce9e3f3d07
gdstout <gdstout27@gmail.com>
2025-01-22 21:09:31 -0500

    update to next photo button

--- index.js
+++ index.js
@@ -1,64 +1,138 @@
 import { ACCESS_KEY_ID, SECRET_ACCESS_KEY } from "./config.js";
 
 AWS.config.update({
-    accessKeyId: ACCESS_KEY_ID,
-    secretAccessKey: SECRET_ACCESS_KEY,
-    region: 'us-east-1',
+  accessKeyId: ACCESS_KEY_ID,
+  secretAccessKey: SECRET_ACCESS_KEY,
+  region: "us-east-1",
 });
 
 const s3 = new AWS.S3();
 
-const bucketName = 'griffinsphotos';
+const bucketName = "griffinsphotos";
 
 let bucketObjects = [];
+let metadata = {};
+let scrambledArray = [];
+let currentIndex = -1;
 
-function getObjects() {
-    const params = { Bucket: bucketName };
+/**
+ * Gets a list of the objects in the bucket and stores them in the bucketObjects array.
+ * Fetches the metadata.json file and stores it in the metadata object.
+ */
+function performSetup() {
+  const params = { Bucket: bucketName };
 
+  try {
     s3.listObjects(params, function (err, data) {
-        if (err) {
-            console.error('Error listing objects: ', err);
-        } else {
-            bucketObjects = data.Contents;
-            pickRandomImg();
-        }
+      if (err) {
+        console.error("Error listing objects: ", err);
+      } else {
+        bucketObjects = data.Contents;
+        scrambledArray = createScrambledArray(bucketObjects.length);
+
+        fetch("https://griffinsphotos.s3.amazonaws.com/metadata.json")
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Error fetching photo metadata.");
+            }
+            return response.json();
+          })
+          .then((data) => {
+            metadata = data;
+            goToNextImage();
+          })
+          .catch((error) => {
+            console.error("Fetch Issue: " + error);
+          });
+      }
     });
+  } catch (err) {
+    console.error("Error during setup: " + err);
+  }
 }
 
-async function pickRandomImg() {
-    if (bucketObjects.length) {
-        try {
-            fetch("https://griffinsphotos.s3.amazonaws.com/metadata.json")
-                .then(response => {
-                    return response.json();
-                })
-                .then(json => {
+/**
+ * Creates a scrambled array of X numbers without duplicates.
+ */
+function createScrambledArray(X) {
+  // Create an array of numbers from 0 to X-1
+  let array = Array.from({ length: X }, (_, i) => i);
 
-                    let index = Math.floor(Math.random() * bucketObjects.length);
-                    const url = "https://griffinsphotos.s3.amazonaws.com/" + bucketObjects[index].Key;
+  // Shuffle the array using the Fisher-Yates algorithm
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 
-                    let img = new Image();
-                    img.onload = function() {
-                        document.getElementById("content").appendChild(img);
-                        document.getElementById("content").className = "content";
-                    }
-                    img.className="main-image"
-                    img.src = url;
-                    try {
-                        document.getElementById("title").innerHTML = json[bucketObjects[index].Key].title;
-                        document.getElementById("description").innerHTML = json[bucketObjects[index].Key].description;
-                    } catch (err) {
-                        console.log("Couldn't get info for this image...");
-                        document.getElementById("title").innerHTML = "Untitled";
-                        document.getElementById("description").innerHTML = "No information."
+  return array;
+}
 
-                    }
-                });
-        } catch (err) {
-            console.log(err);
-        }
+export async function goToNextImage() {
+  document.getElementById("content").className = "content-hidden";
 
+  if (currentIndex < bucketObjects.length) {
+    currentIndex ++;
+  } else {
+    currentIndex = 0;
+  }
+
+  const url =
+    "https://griffinsphotos.s3.amazonaws.com/" +
+    bucketObjects[scrambledArray[currentIndex]].Key;
+
+  let img = document.getElementById("main-image");
+  img.src = url;
+  img.onload = function () {
+    document.getElementById("content").className = "content";
+  }
+
+
+  try {
+    document.getElementById("title").innerHTML =
+      metadata[bucketObjects[scrambledArray[currentIndex]].Key].title;
+    document.getElementById("description").innerHTML =
+      metadata[bucketObjects[scrambledArray[currentIndex]].Key].description;
+  } catch (err) {
+    console.error("Couldn't find metadata for this image...");
+    document.getElementById("title").innerHTML = "Untitled";
+    document.getElementById("description").innerHTML = "No information.";
+  }
+}
+window.goToNextImage = goToNextImage;
+
+/**
+ * This section defines variables and functions to determine if a mobile
+ * user has swiped to see the next picture.
+ */
+let touchStartX = 0;
+let touchEndX = 0;
+let touchStartTime = null;
+let touchDuration = null;
+
+function checkDirection() {
+  if (touchDuration <= 300) {
+    if (touchEndX < touchStartX) {
+      pickRandomImg();
     }
+    if (touchEndX > touchStartX) {
+      pickRandomImg();
+    }
+  }
 }
 
-getObjects();
+document.addEventListener("touchstart", (e) => {
+  touchStartX = e.changedTouches[0].screenX;
+  touchStartTime = new Date().getTime();
+});
+
+document.addEventListener("touchend", (e) => {
+  touchDuration = new Date().getTime() - touchStartTime;
+  touchEndX = e.changedTouches[0].screenX;
+  checkDirection();
+});
+
+
+/**
+ * Initiate the first load of data
+ */
+performSetup();
