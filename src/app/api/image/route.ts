7d74993da7ee84860228d9785fef343ada31eea7
gdstout <gdstout27@gmail.com>
2026-09-12 14:07:10 -0400

    remove credential injection

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -3,10 +3,6 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { NextRequest, NextResponse } from "next/server";
 
 const s3 = new S3Client({
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-    },
     region: process.env.AWS_REGION,
 });
 
--- src/app/api/metadata/route.ts
+++ src/app/api/metadata/route.ts
@@ -1,28 +1,29 @@
 import { ImageMetadata } from "@/src/lib/types";
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3"
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { NextResponse } from "next/server";
 
 const s3 = new S3Client({
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-    },
-    region: process.env.AWS_REGION
+  region: process.env.AWS_REGION,
 });
 
 export async function GET() {
-    try {
-        const command = new GetObjectCommand({
-            Bucket: process.env.AWS_BUCKET_NAME,
-            Key: "metadata.json",
-        });
+  try {
+    const command = new GetObjectCommand({
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: "metadata.json",
+    });
 
-        const res = await s3.send(command);
-        const metadata: ImageMetadata[] = JSON.parse(await res.Body!.transformToString());
+    const res = await s3.send(command);
+    const metadata: ImageMetadata[] = JSON.parse(
+      await res.Body!.transformToString(),
+    );
 
-        return NextResponse.json(metadata);
-    } catch (error) {
-        console.error("Error fetching metadata from S3: ", error);
-        return NextResponse.json({error: "Error fetching metadata!"}, {status: 500})
-    }
+    return NextResponse.json(metadata);
+  } catch (error) {
+    console.error("Error fetching metadata from S3: ", error);
+    return NextResponse.json(
+      { error: "Error fetching metadata!" },
+      { status: 500 },
+    );
+  }
 }
