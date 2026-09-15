b65364ae3316c0f8b7f6fede4d4716c38220db53
gdstout <gdstout27@gmail.com>
2026-09-15 17:07:54 -0400

    small updates

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -10,10 +10,10 @@ export default function Header() {
     <div className="flex border-b border-dotted border-b-taupe-300">
       <Link
         href="/"
-        className={`border-r ${pathname.includes("all") && "border-none"} [@media(max-aspect-ratio:3/4)]:text-s border-dotted border-b-taupe-300 px-4 py-1 font-bold transition duration-1000 hover:bg-taupe-300 hover:text-taupe-950 hover:duration-150`}
+        className={`border-r ${pathname.includes("all") && "border-none"} [@media(max-aspect-ratio:3/4)]:text-s border-dotted border-b-taupe-300 px-4 py-1 font-bold transition duration-1000 hover:bg-taupe-300 hover:text-taupe-950 hover:duration-150 [@media(max-aspect-ratio:3/4)]:border-none`}
       >
-        Griffin Stout Photography{" "}
-        {pathname.includes("admin") || (pathname.includes("login") && "Admin")}
+        Griffin Stout Photography {pathname.includes("admin") && "Admin"}
+        {pathname.includes("login") && "Login"}
       </Link>
       {!pathname.includes("all") && (
         <Link
