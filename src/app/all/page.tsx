632bf43d821b974db25b08901c95b1ded2c948ff
gdstout <gdstout27@gmail.com>
2026-09-15 10:45:15 -0400

    force dynamic

--- src/app/all/page.tsx
+++ src/app/all/page.tsx
@@ -3,6 +3,8 @@ import { ImageMetadata } from "@/src/lib/types";
 import { shuffle } from "../page";
 import GalleryAll from "../(components)/GalleryAll";
 
+export const dynamic = "force-dynamic";
+
 export default async function All() {
   const metadata: ImageMetadata[] = await getMetadata();
   shuffle(metadata);
