d1880bbe7b32d72559b3d089e56e406130a657ff
gdstout <gdstout27@gmail.com>
2026-09-16 16:54:08 -0400

    revert commit destroying layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import Header from "./(components)/Header";
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Griffin Stout Photography",
+  description: "Film photography by Griffin Stout",
+  openGraph: {
+    title: "Griffin Stout Photography",
+    description: "Film photography by Griffin Stout",
+    type: "website",
+  },
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html lang="en">
+      <body className="flex h-screen flex-col bg-taupe-950 font-serif text-taupe-300">
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
